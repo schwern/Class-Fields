05a8ae73c25fd4a918cc430b353bacb8b2a59e81
Michael G. Schwern <schwern@pobox.com>
1999-12-08 21:13:51 +0000

    Added docs
    git-svn-id: file:///Users/schwern/tmp/svn/CPAN/Class-Fields/trunk@2660 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/private.pm
+++ lib/private.pm
@@ -25,3 +25,67 @@ sub import {
 
 
 return 'pants of infinity';
+__END__
+=pod
+
+=head1 NAME
+
+  private - Add private data members to Perl classes
+
+
+=head1 SYNOPSIS
+
+  package GI::Joe;
+
+  use private qw( _SexualPrefs _IsSpy );
+
+
+=head1 DESCRIPTION
+
+private.pm adds a list of keys as private data members to the current
+class.  See L<public> for more info.
+
+Private data members are those pieces of data which are expected to be
+only accessed by methods of the class which owns them.  They are not
+inherited by subclasses.
+
+private.pm serves a subset of the functionality of fields.pm.
+
+  use private qw(_Foo);
+
+is almost exactly the same as:
+
+  use fields qw(_Foo);
+
+with the exception that you can (if you REALLY want to) do something
+like this:
+
+  use private qw(Foo);
+
+Whereas one cannot do this with fields.pm. (Note: This is considered
+unwise and private.pm will scream about it if you have Perl's warnings
+on.)
+
+Additionally, private.pm is a bit clearer in its intent and is not
+necessarily implying use of pseudo-hashes.
+
+
+=head1 MUSINGS
+
+I fully expect private.pm to eventually mutate into a real pragma
+someday when a better formalized OO data system for Perl supplants the
+current fledgling pseudo-hashes.
+
+=head1 HISTORY
+
+This module was added as of version 5.6
+
+=head1 AUTHOR
+
+Michae G Schwern <schwern@pobox.com>
+
+=head1 SEE ALSO
+
+L<public>, L<protected>, L<fields>, L<base>, L<Class::Fields::Inspector>
+
+=cut
--- lib/public.pm
+++ lib/public.pm
@@ -25,3 +25,70 @@ sub import {
 
 
 return 'Do not forget to **enjoy the path**';
+__END__
+=pod
+
+=head1 NAME
+
+  public - Add public data members to Perl classes
+
+
+=head1 SYNOPSIS
+
+  package GI::Joe;
+
+  use public qw( Name Rank Serial_Number );
+
+
+=head1 DESCRIPTION
+
+public.pm adds a list of keys as public data members to the current
+class.  This is useful when using pseudo-hashes as objects, or for
+simply imposing a bit more structure on your Perl objects than is
+normally expected.  It allows you to use the methods provided in
+Class::Fields::Inspector.
+
+Public data members are those pieces of data which are expected to be
+regularly accessed by methods, functions and programs outside the
+class which owns them.  They are also inherited by any subclasses.
+
+public.pm serves a subset of the functionality of fields.pm.
+
+  use public qw(Foo);
+
+is almost exactly the same as:
+
+  use fields qw(Foo);
+
+with the exception that you can (if you REALLY want to) do something
+like this:
+
+  use public qw(_Foo);
+
+Whereas one cannot do this with fields.pm. (Note: This is considered
+unwise and public.pm will scream about it if you have Perl's warnings
+on.)
+
+Additionally, public.pm is a bit clearer in its intent and is not
+necessarily implying use of pseudo-hashes.
+
+
+=head1 MUSINGS
+
+I fully expect public.pm to eventually mutate into a real pragma
+someday when a better formalized OO data system for Perl supplants the
+current fledgling pseudo-hashes.
+
+=head1 HISTORY
+
+This module was added as of version 5.6
+
+=head1 AUTHOR
+
+Michae G Schwern <schwern@pobox.com>
+
+=head1 SEE ALSO
+
+L<private>, L<protected>, L<fields>, L<base>, L<Class::Fields::Inspector>
+
+=cut
