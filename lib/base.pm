cd19658d2c761eca6778626e0395f62d348cd182
Michael G. Schwern <schwern@pobox.com>
1999-12-15 02:40:02 +0000

    Version 1.92
    Using Class::Fields::Fuxor and the new Class::Fields.
    
    Allow multiple inheritance of classes which have fields but have no
    inheritable fields.
    
    
    git-svn-id: file:///Users/schwern/tmp/svn/CPAN/Class-Fields/trunk@2669 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/base.pm
+++ lib/base.pm
@@ -1,57 +1,66 @@
 package base;
 
 use vars qw($VERSION);
-$VERSION = "1.91";
+$VERSION = "1.92";
 
 use Class::Fields::Inherit;
-use Class::Fields qw(:Fields);
+use Class::Fields::Fuxor;
+use Class::Fields;
 
-use constant SUCCESS => 1;
+use constant SUCCESS => (1==1);
+use constant FAILURE => !SUCCESS;
 
 sub import {
     my $class = shift;
 
-	return SUCCESS unless @_;
+    return SUCCESS unless @_;
 
-	# List of base classes from which we will inherit %FIELDS.
-	my $fields_base;
+    # List of base classes from which we will inherit %FIELDS.
+    my $fields_base;
 
     my $inheritor = caller(0);
 
     foreach my $base (@_) {
-		unless (exists ${"$base\::"}{VERSION}) {
-	        eval "require $base";
-			# Only ignore "Can't locate" errors from our eval require.
-			# Other fatal errors (syntax etc) must be reported.
-			die if $@ && $@ !~ /^Can't locate .*? at \(eval /; #'#
-			unless (%{"$base\::"}) {
-				require Carp;
-				Carp::croak("Base class package \"$base\" is empty.\n",
-							"\t(Perhaps you need to 'use' the module ",
-							"which defines that package first.)");
-			}
-			${"$base\::"}{VERSION} = "-1, set by base.pm"
-			  unless exists ${"$base\::"}{VERSION};
+        unless (exists ${"$base\::"}{VERSION}) {
+            eval "require $base";
+            # Only ignore "Can't locate" errors from our eval require.
+            # Other fatal errors (syntax etc) must be reported.
+            die if $@ && $@ !~ /^Can't locate .*? at \(eval /; #'#
+            unless (%{"$base\::"}) {
+                require Carp;
+                Carp::croak("Base class package \"$base\" is empty.\n",
+                            "\t(Perhaps you need to 'use' the module ",
+                            "which defines that package first.)");
+            }
+            ${"$base\::"}{VERSION} = "-1, set by base.pm"
+              unless exists ${"$base\::"}{VERSION};
         }
 
         # A simple test like (defined %{"$base\::FIELDS"}) will
         # sometimes produce typo warnings because it would create
         # the hash if it was not present before.
-        my $fglob;
-        if ( has_fields($base) ) {
+        #
+        # We don't just check to see if the base in question has %FIELDS
+        # defined, we also check to see if it has -inheritable- fields.
+        # Its perfectly alright to inherit from multiple classes that have 
+        # %FIELDS as long as only one of them has fields to give.
+        if ( has_fields($base) and 
+             ( show_fields($base, 'Public') or 
+               show_fields($base, 'Protected') )
+           ) {
             # No multiple fields inheritence *suck*
-			if ($fields_base) {
-				require Carp;
-				Carp::croak("Can't multiply inherit %FIELDS");
-			} else {
-				$fields_base = $base;
-			}
-		}
+            if ($fields_base) {
+                require Carp;
+                Carp::croak("Can't multiply inherit %FIELDS");
+            } else {
+                $fields_base = $base;
+            }
+        }
     }
 
     if( defined $fields_base ) {
-		inherit_fields($inheritor, $fields_base);
-	}
+        inherit_fields($inheritor, $fields_base);
+    }
 
     push @{"$inheritor\::ISA"}, @_;
 }
@@ -74,15 +83,15 @@ base - Establish IS-A relationship with base class at compile time
 Roughly similar in effect to
 
     BEGIN {
-		require Foo;
-		require Bar;
-		push @ISA, qw(Foo Bar);
+        require Foo;
+        require Bar;
+        push @ISA, qw(Foo Bar);
     }
 
-Will also initialize the %FIELDS hash if one or more of the base
-classes has it using all public and protected data members of the base
-classes.  Multiple Inheritence is supported.  If two or more base
-classes each wish to endow the same fields, the 'base' pragma will
+Will also initialize the %FIELDS hash if one of the base classes has
+it using all public and protected data members of the base classes.
+Multiple Inheritence of fields is B<NOT> supported.  If two or more
+base classes each have inheritable fields the 'base' pragma will
 croak.  See L<fields>, L<public> and L<protected> for a description of
 this feature.
 
@@ -100,8 +109,9 @@ C<-1, defined by base.pm>.
 
 This module was introduced with Perl 5.004_04.
 
+
 =head1 SEE ALSO
 
-L<fields>, L<public>, L<protected>
+L<fields>, L<public>, L<protected>, L<protected>, L<Class::Fields>
 
 =cut
