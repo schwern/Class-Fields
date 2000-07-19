28e30ffe076a570a3aeca92f4519fd33a85f811b
Michael G. Schwern <schwern@pobox.com>
2000-07-19 06:40:29 +0000

    Version 0.13
    Added 5.005 check (it might work eariler).
    
    Now using add_field_set() to make sure fields get added in the right order.
    
    Brought the docs up to date with 5.6.0 with some minor changes.
    
    Added fields::new() and fields::phash() from 5.6.0's fields.pm
    
    Added NOTE about this not being perl's fields.pm.  Also noted its
    fully compatible with perl's fields.pm now.
    
    Added AUTHOR
    
    Now fully compatible with 5.6.0's fields.pm (passes regression tests).
    
    
    git-svn-id: file:///Users/schwern/tmp/svn/CPAN/Class-Fields/trunk@2706 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/fields.pm
+++ lib/fields.pm
@@ -1,31 +1,39 @@
 package fields;
 
+use 5.005;
 use strict;
+no strict 'refs';
 use vars qw($VERSION);
 
 use Class::Fields::Fuxor;
 use Class::Fields::Attribs;
+use Carp::Assert;
 
 use constant SUCCESS => 1;
 
-$VERSION = "0.12";
+$VERSION = "0.13";
 
 sub import {
-    # Dump the class.
-    shift;
+    my($class, @fields) = @_;
     
     my $package = caller(0);
     
-    return SUCCESS unless @_;
+    return SUCCESS unless @fields;
 
-    add_fields($package, PRIVATE, grep {/^_/} @_);
-    add_fields($package, PUBLIC,  grep {!/^_/} @_);
+    my @attribs = ();
+    foreach my $field (@fields) {
+        my $attr = ($field =~ /^_/) ? PRIVATE : PUBLIC;
+
+        push @attribs, $attr;
+    }
+
+    assert(@fields == @attribs) if DEBUG;
+
+    # Can't use add_fields() since fields.pm needs them all at once
+    # for magical reasons.  Also preserves field ordering.
+    add_field_set($package, \@fields, \@attribs);
 }
 
-
-1;
-
-__END__
 =pod
 
 =head1 NAME
@@ -34,65 +42,218 @@ fields - compile-time class fields
 
 =head1 SYNOPSIS
 
-    {
-        package Foo;
-        use fields qw(foo bar _private);
-    }
-    ...
-    my Foo $var = new Foo;
-    $var->{foo} = 42;
+  package Foo;
+  use fields qw(foo bar _Foo_private);
 
-    # This will generate a compile-time error.
-    $var->{zap} = 42;
+  sub new {
+      my $proto = shift;
+      my $class = ref $proto || $proto;
+      
+      my Foo $self = fields::new($class);
 
-    {
-        package Bar;
-        use base 'Foo';
-        use fields 'bar';             # hides Foo->{bar}
-        use fields qw(baz _private);  # not shared with Foo
-    }
+      $self->{_Foo_private} = "this is Foo's secret";
+      $self->{foo} = 'everybody knows';
+      $self->{bar} = 'an open bar';
+
+      return $self;
+  }
+
+  my Foo $foo_obj = Foo->new;
+  $foo_obj->{foo} = 42;
+
+  # This will generate a compile-time error.  zap is not a
+  # public field of Foo.
+  $foo_obj->{zap} = 42;
+
+
+  # subclassing
+  package Bar;
+  use base 'Foo';
+  use fields qw(baz _Bar_private);  # fields not shared with foo.
+
+  sub new {
+      my $proto = shift;
+      $self = $proto->SUPER::new;  # call Foo's new()
+
+      $self->{baz} = 'and stuff';  # initialize my own fields.
+      $self->{_Bar_private} = 'our little secret';
+
+      return $self;
+  }
+
 
 =head1 DESCRIPTION
 
-The C<fields> pragma enables compile-time verified class fields.  It
-does so by updating the %FIELDS hash in the calling package.
+The C<fields> pragma enables compile-time verified class fields.
 
-If a typed lexical variable holding a reference is used to access a
-hash element and the %FIELDS hash of the given type exists, then the
-operation is turned into an array access at compile time.  The %FIELDS
-hash maps from hash element names to the array indices.  If the hash
-element is not present in the %FIELDS hash, then a compile-time error
-is signaled.
+NOTE: The current implementation keeps the declared fields in the %FIELDS
+hash of the calling package, but this may change in future versions.
+Do B<not> update the %FIELDS hash directly, because it must be created
+at compile-time for it to be fully useful, as is done by this pragma.
 
-Since the %FIELDS hash is used at compile-time, it must be set up at
-compile-time too.  This is made easier with the help of the 'fields'
-and the 'base' pragma modules.  The 'base' pragma will copy fields
-from base classes and the 'fields' pragma adds new fields.  Field
-names that start with an underscore character are made private to a
-class and are not visible to subclasses.  Inherited fields can be
+If a typed lexical variable (my Dog $spot) holding a reference is used
+to access a hash element and a package/class with the same name as the
+type has declared class fields using this pragma, then the operation
+is turned into an array access at compile time.
+(L<perlop/"Pseudo-hashes: Using an array as a hash")
+
+The relatied C<base> pragma will combine fields from base classes and
+any fields declared using the C<fields> pragma.  This enables field
+inheritance to work properly.
+
+Field names that start with an underscore character are made private
+to a class and are not visible to subclasses.  Inherited fields can be
 overridden but will generate a warning if used together with the C<-w>
 switch.
 
 The effect of all this is that you can have objects with named fields
-which are as compact and as fast arrays to access.  This only works
-as long as the objects are accessed through properly typed variables.
-For untyped access to work you have to make sure that a reference to
-the proper %FIELDS hash is assigned to the 0'th element of the array
-object (so that the objects can be treated like an pseudo-hash).  A
-constructor like this does the job:
+which are as compact and as fast arrays to access.  This only works as
+long as the objects are accessed through properly typed variables.  If
+the objects are not typed, access is only checked at run-time and
+performance may suffer a bit.
 
-  sub new
-  {
-      my $class = shift;
-      no strict 'refs';
-      my $self = bless [\%{$class.'::FIELDS'}], $class;
-      return $self;
-  }
+=head2 Functions
 
+=over 4
+
+=item B<new>
+
+  $obj = fields::new($class);
+  $obj = fields::new($another_obj);
+
+fields::new() creates and blesses a pseudo-hash comprised of the
+fields declared using the C<fields> pragma into the specified class.
+This makes it possible to write a constructor like this:
+
+    package Critter::Sounds;
+    use fields qw(cat dog bird);
+
+    sub new {
+        my $proto = shift;
+        my $class = ref $proto || $proto;
+
+        my Critter::Sounds $self = fields::new($class);
+
+        %$self = (
+                  cat     => 'meow',
+                  dog     => 'bark',
+                  dogcow  => 'moof',
+                 );
+        
+        return $self;
+    }
+
+=cut
+
+sub new {
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+    return bless [\%{$class . "::FIELDS"}], $class;
+}
+
+=pod
+
+=item B<phash>
+
+    $phash = fields::phash;
+    $phash = fields::phash(\@keys);
+    $phash = fields::phash(\@keys, \@values);
+    $phash = fields::phash(%hash);
+
+fields::phash() can be used to create and initialize a plain
+(unblessed) pseudo-hash.  It is prefered that this function be used
+instead of creating pseudo-hashes directly.
+
+If no arguments are given the resulting pseudohash will be empty and
+have no fields.
+
+The optional @keys will be used to initialize the keys/fields of the
+resulting hash.  @values, also optional, will be used as the values
+for each key.  If @values contains less elements than @keys, the
+trailing elements of the pseudo-hash will not be initialized.  If
+there are more @values than @keys the function will throw a warning
+(it may die in the future).
+
+This makes it particularly useful for creating a pseudo-hash from
+subroutine arguments.
+
+    sub dogtag {
+        my $tag = fields::phash([qw(name rank serial_num)], [@_]);
+    }
+
+fields::phash() also accepts a plain %hash used to construct the
+pseudo-hash.  Examples:
+
+    my $tag = fields::phash(name       => 'Kirk',
+                            rank       => 'Captain',
+                            serial_num => 42
+                           );
+
+    my $phash = fields::phash(%args);
+
+=cut
+
+sub phash {
+    my $keys = {};
+    my $values = [];
+
+    if( ref $_[0] ) {   # Called as phash(\@keys, \@values)
+        my($keys_in, $vals_in) = @_;
+
+        @{$keys}{@$keys_in} = 1..@$keys_in;
+        $values = $vals_in if defined $vals_in;
+
+        if( @_ > 2 ) {      # sanity check
+            require Carp;
+            Carp::croak("Expected at most two array refs.");
+        }
+    }
+    else {
+        if( @_ % 2 ) {
+            require Carp;
+            Carp::croak("Odd number of elements initializing pseudo-hash.");
+        }
+
+        my $i = 0;
+        @$keys{grep ++$i %2, @_} = 1 .. @_ / 2;
+
+        $i = 0;
+        $values = [grep $i++ % 2, @_];
+    }
+
+    # Make sure we didn't get too many values.
+    if( @$values > keys %$keys ) {
+        require Carp;
+        Carp::carp("More values than keys were given.");
+    }
+    
+    return [$keys, @$values];
+}
+
+=pod
+
+=back
 
 =head1 SEE ALSO
 
 L<base>, L<public>, L<private>, L<protected>, L<Class::Fields>
 L<perlref/Pseudo-hashes: Using an array as a hash>
 
+=head1 B<NOTE>
+
+This is the version of fields.pm which comes with Class::Fields.  NOT
+the version which is distributed with Perl.  This version should
+safely emulate everything that perl 5.6.0's fields.pm does.  It passes
+all of 5.6.0's regression tests.
+
+It should also work under 5.005_03, although if you're going to be
+screwing around with pseudohashes you really should upgrade to 5.6.0.
+
+=head1 AUTHOR
+
+Michael G Schwern <schwern@pobox.com>.  fields::new(), fields::phash()
+and most of the documentation taken from the original fields.pm.
+
 =cut
+
+1;
